e8c1a6cb57b91b8ca8c64e9e3172325ef8c300af
Thomas Quig <trquig1@gmail.com>
2017-06-30 08:23:26 -0700

    Started working on the game manager and pause menu

---
+++ A to B/Assets/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject inGameMenu;
+	public GameObject pauseMenu;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
