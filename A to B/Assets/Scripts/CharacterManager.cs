521753d526114b6fdb98d0f732edb647887555de
Thomas Quig <trquig1@gmail.com>
2017-06-29 19:51:04 -0700

    Added a font folder, slow motion, and filled all folders

---
+++ A to B/Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterManager : MonoBehaviour {
+	public float slowRatio;
+	public Image image;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Mouse0)) {
+			image.gameObject.SetActive(true);
+			Time.timeScale = slowRatio;
+		} else {
+			Time.timeScale = 1.0f;
+			image.gameObject.SetActive(false);
+		}
+	}
+}
