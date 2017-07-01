af64c604834c30cb8430ce163d490cf14eaec8c6
Thomas Quig <trquig1@gmail.com>
2017-06-30 21:47:09 -0700

    Added working Level Navigation, A Main Menu Scene, a default level with assets etc.

--- A To B/Assets/Scripts/WorldManager.cs
+++ A To B/Assets/Scripts/WorldManager.cs
@@ -3,31 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+public class WorldManager : MonoBehaviour {
 
 	public GameObject inGameMenu;
 	public GameObject pauseMenu;
+	public string[] levels;
+	public static string[] statLevels;
+	public static int currentLevel;
 	private bool paused;
+	public static bool pausedStat;
+
 	// Use this for initialization
 	void Start () {
 		paused = false;
+		pausedStat = false;
+		statLevels = levels;
+		if(SceneManager.GetActiveScene().name.Equals("Level 1"))
+			{
+				currentLevel = 1;
+			}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Escape)) {
 			paused = !paused;
+			pausedStat = !pausedStat;
+			if (paused) {
+				Time.timeScale = 0.0f;
+				pauseMenu.gameObject.SetActive (true);
+				inGameMenu.gameObject.SetActive (false);
+			} 
+			else 
+			{
+				Time.timeScale = 1.0f;
+				pauseMenu.gameObject.SetActive (false);
+				inGameMenu.gameObject.SetActive (true);
+			}
 		}
+			
 
-		if (paused) {
-			Time.timeScale = 0.0f;
-			pauseMenu.gameObject.SetActive (true);
-			inGameMenu.gameObject.SetActive (false);
-		} else {
-			Time.timeScale = 1.0f;
-			pauseMenu.gameObject.SetActive (false);
-			inGameMenu.gameObject.SetActive (true);
-		}
+	}
+		
+	public static void goToLevel(int pos)
+	{
+		SceneManager.LoadScene (statLevels [pos % statLevels.Length]);
+	}
 
+	public static void nextLevel()
+	{
+		currentLevel++;
+		SceneManager.LoadScene (statLevels[currentLevel % statLevels.Length]);
+	}
+
+	public static bool isPaused()
+	{
+		return pausedStat;
+	}
+
+	public static void setCurrentLevel(int lev)
+	{
+		currentLevel = lev % statLevels.Length;
 	}
 }
--- A to B/Assets/Scripts/CharacterManager.cs
+++ A to B/Assets/Scripts/CharacterManager.cs
@@ -13,14 +13,31 @@ public class CharacterManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (!WorldManager.isPaused ()) {
-			if (Input.GetKey (KeyCode.Mouse0)) {
-				image.gameObject.SetActive (true);
+		if (!WorldManager.isPaused()) {
+			if (Input.GetKey(KeyCode.Mouse0)) {
 				Time.timeScale = slowRatio;
-			} else if (Input.GetKeyUp (KeyCode.Mouse0)) {
+				Debug.Log (Time.timeScale);
+				image.gameObject.SetActive (true);
+
+			}
+			else if (Input.GetKeyUp (KeyCode.Mouse0)) {
 				Time.timeScale = 1.0f;
 				image.gameObject.SetActive (false);
 			}
 		}
+		if (transform.position.y < -30) {
+			transform.position = new Vector3 (0, 5, 0);
+		}
+			
+	}
+
+	public void OnTriggerEnter(Collider col)
+	{
+		Debug.Log ("ENTER");
+		if(col.gameObject.tag.Equals("FinishPad"))
+		{
+			WorldManager.nextLevel ();
+		}
+			
 	}
 }
